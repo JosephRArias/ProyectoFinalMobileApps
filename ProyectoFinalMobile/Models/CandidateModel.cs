9e0edc621a25d68afc81db3abf010c0a2a34d6ce
Jose Arias <joseam2969@gmail.com>
2019-09-27 16:48:23 -0400

    Finished Some Necessary models

---
+++ ProyectoFinalMobile/Models/CandidateModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ProyectoFinalMobile.Models
+{
+	public class CandidateModel
+	{
+		public string Formatted_address { get; set; }
+		public string Name { get; set; }
+		public int Price_level { get; set; }
+	}
+}
---
+++ ProyectoFinalMobile/Models/UserModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProyectoFinalMobile.Models
+{
+	public class UserModel
+	{
+		
+		int IDCard { get; set; }
+		string Username { get; set; }
+		string Password { get; set; }
+	}
+}

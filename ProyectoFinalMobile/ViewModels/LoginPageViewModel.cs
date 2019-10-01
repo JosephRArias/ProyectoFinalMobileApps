7fff32e52d87ab92073e2d7d31c92b3b9cf14eea
Jose Arias <joseam2969@gmail.com>
2019-10-01 12:28:47 -0400

    Fixing issues with compiler

---
+++ ProyectoFinalMobile/ViewModels/LoginPageViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProyectoFinalMobile.ViewModels
+{
+	public class LoginPageViewModel
+	{
+		public LoginPageViewModel()
+		{
+
+		}
+
+	}
+}
---
+++ ProyectoFinalMobile/ViewModels/SignupPageViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProyectoFinalMobile.ViewModels
+{
+	public class SignupPageViewModel
+	{
+		public SignupPageViewModel()
+		{
+
+		}
+	}
+}

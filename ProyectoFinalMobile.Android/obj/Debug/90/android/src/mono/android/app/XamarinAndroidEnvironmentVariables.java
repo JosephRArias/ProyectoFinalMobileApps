9cdd0e8e8f7282e00d9a0165f81d813da698a777
Jose Arias <joseam2969@gmail.com>
2019-10-03 09:03:48 -0400

    Configured basic Prism Navigation

--- ProyectoFinalMobile.Android/obj/Debug/90/android/src/mono/android/app/XamarinAndroidEnvironmentVariables.java
+++ ProyectoFinalMobile.Android/obj/Debug/90/android/src/mono/android/app/XamarinAndroidEnvironmentVariables.java
@@ -5,7 +5,7 @@ public class XamarinAndroidEnvironmentVariables
 	// Variables are specified the in "name", "value" pairs
 	public static final String[] Variables = new String[] {
 		"MONO_LOG_LEVEL", "info",
-		"XAMARIN_BUILD_ID", "8dba117d-9da5-4398-ac61-b61db513e16f",
+		"XAMARIN_BUILD_ID", "55205bb9-91e6-4674-86f9-51719bb71dca",
 		"XA_HTTP_CLIENT_HANDLER_TYPE", "Xamarin.Android.Net.AndroidClientHandler",
 		"XA_TLS_PROVIDER", "btls",
 		"MONO_GC_PARAMS", "major=marksweep-conc",
